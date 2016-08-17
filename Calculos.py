41486ca8630759f6589752f74f7638ea84712380
Diego de França <ufpb.diego@gmail.com>
2016-08-17 14:19:27 -0300

    Add files via upload

---
+++ Calculos.py
@@ -0,0 +1,10 @@
+soma = 200 + 250
+print ('Soma 200 + 250 = ' , soma)
+divisao = 200 / 8
+print ('Divisao 200 / 8 = ' , divisao)
+multiplicacao = 200 * 7
+print ('Multiplicacao 200*7 = ', multiplicacao)
+subtracao = 800 - 10
+print ('Subtracao 800 - 10 = ' , subtracao)
+potencia = 2 ** 10
+print('Potencia 2^10 = ' , potencia)
